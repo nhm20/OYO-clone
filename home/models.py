7bbb4c45fbf531324a0c59c56badc45e397463b7
nmo20 <nmo212182@gmail.com>
2025-08-22 23:02:21 +0530

    Relationship between Models

--- home/models.py
+++ home/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=100)
+
 class Student(models.Model):
+    # Choose ONE of these relationship types:
+    
+    # ForeignKey (many-to-one)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    
+    # OR OneToOneField (one-to-one) 
+    # department = models.OneToOneField(Department, on_delete=models.CASCADE)
+    
+    # OR ManyToManyField (many-to-many)
+    # department = models.ManyToManyField(Department)
+
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(max_length=10, default="Male")
     phone_number = models.CharField(max_length=10)
     email = models.EmailField()
     date_of_birth = models.DateField()
-    student_registration = models.DateField()
-    percentage = models.FloatField()
-    student_image=models.ImageField(upload_to='images/students/', null=True, blank=True)
-    file=models.FileField(upload_to='files/students/')
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    slug=models.SlugField()
-    uid=models.UUIDField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
