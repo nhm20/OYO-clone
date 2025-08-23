a8d4ac20c7810654007c7ed5eb60ac4817d6d0ef
nmo20 <nmo212182@gmail.com>
2025-08-23 21:17:03 +0530

    Null Vs blank

--- home/models.py
+++ home/models.py
@@ -7,34 +7,11 @@ class Department(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    age = models.IntegerField(null=True,blank=True)
+    # null is for DB,blank is for Django
     gender = models.CharField(max_length=10, default="Male")
     phone_number = models.CharField(max_length=10)
     email = models.EmailField()
     date_of_birth = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  
-
-student=Student.objects.create(name="Abhijeet", age=23, gender="Male", phone_number="3453453454", email="sdf@gamil.com", date_of_birth="2212-03-03")
-
-student.save()
-
-student=Student.objects.get(id=1)
-
-print(student.name)
-
-student=Student.objects.all()
-print(student)
-
-student=Student.objects.filter(name="Abhijeet")
-print(student)
-
-# update
-student.age=24
-student.save()
-
-# retrieve
-student=Student.objects.get(id=1)
-student.delete()
-
-student=Student.objects.all().delete()
