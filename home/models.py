d34a1c0c53ac5ecf854a1c7c5eb9d0d5733c3295
nmo20 <nmo212182@gmail.com>
2025-08-23 21:33:07 +0530

    Relationships

--- home/models.py
+++ home/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class College(models.Model):
+    college_name = models.CharField(max_length=100)
 
 class Department(models.Model):
     department_name = models.CharField(max_length=100)
 
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=100)
+
+
 class Student(models.Model):
+    college= models.OneToOneField(College,on_delete=models.CASCADE)
+    department = models.ForeignKey(Department,on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skills)
     name = models.CharField(max_length=100)
-    age = models.IntegerField(null=True,blank=True)
-    # null is for DB,blank is for Django
-    gender = models.CharField(max_length=10, default="Male")
-    phone_number = models.CharField(max_length=10)
-    email = models.EmailField()
-    date_of_birth = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  
